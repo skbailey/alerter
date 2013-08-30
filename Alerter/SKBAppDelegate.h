81117b4b6c1771a8d476507e665ab9cd3493c3a2
Sherard Bailey <sherard@gmail.com>
2013-08-30 12:09:54 -0400

    Initial Commit

---
+++ Alerter/SKBAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  SKBAppDelegate.h
+//  Alerter
+//
+//  Created by shbailey63 on 8/30/13.
+//  Copyright (c) 2013 shbailey63. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class SKBViewController;
+
+@interface SKBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) SKBViewController *viewController;
+
+@end
---
+++ Alerter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Alerter
+//
+//  Created by shbailey63 on 8/30/13.
+//  Copyright (c) 2013 shbailey63. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SKBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([SKBAppDelegate class]));
+  }
+}
